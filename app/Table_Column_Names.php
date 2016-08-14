7920bb486aa0260e576a283ea5485dc4ffd047ac
GaddielTechnologiesGIT <kvteak@gmail.com>
2016-08-15 04:21:47 +0530

    Add files via upload

---
+++ app/Table_Column_Names.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Table_Column_Names  extends Model
+{
+  protected $table ="TABLE_COLUMN_NAMES";
+  const CREATED_AT = 'CREATEDDATE';
+  const UPDATED_AT = 'LASTUPDATEDATE';
+}
